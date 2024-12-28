b52e780ca91b05288dbb65d372de68b777a75ae8
samrudhi <samrudhi.patil@merkle.com>
2024-12-28 12:25:33 +0530

    login pages

--- src/components/cp-login/range.js
+++ src/components/cp-login/range.js
@@ -1,4 +1,3 @@
-
 var sheet = document.createElement('style');
 var rangeInput = document.querySelectorAll('.range input');
 var prefs = ['webkit-slider-runnable-track', 'moz-range-track', 'ms-track'];
@@ -7,7 +6,7 @@ document.body.appendChild(sheet);
 
 var getTrackStyle = function (el) {
   var curVal = el.value;
-  var val = (curVal - 1) * 16.666666667;
+  var val = ((curVal - 1) / (el.max - 1)) * 100; // Calculate percentage
   var style = '';
 
   // Set active label
@@ -17,17 +16,18 @@ var getTrackStyle = function (el) {
   });
 
   var curLabel = document.querySelector('.range-labels li:nth-child(' + curVal + ')');
-
-  curLabel.classList.add('active', 'selected');
-  var prevLabels = Array.from(curLabel.previousElementSibling);
-  prevLabels.forEach(function (label) {
-    label.classList.add('selected');
-  });
+  if (curLabel) {
+    curLabel.classList.add('active', 'selected');
+    var prevLabels = Array.from(curLabel.parentElement.children).slice(0, curVal - 1);
+    prevLabels.forEach(function (label) {
+      label.classList.add('selected');
+    });
+  }
 
   // Change background gradient
   for (var i = 0; i < prefs.length; i++) {
-    style += '.range {background: linear-gradient(to right, #37adbf 0%, #37adbf ' + val + '%, #fff ' + val + '%, #fff 100%)}';
-    style += '.range input::-'+ prefs[i] + '{background: linear-gradient(to right, #37adbf 0%, #37adbf ' + val + '%, #b2b2b2 ' + val + '%, #b2b2b2 100%)}';
+    style += `.range {background: linear-gradient(to right, #670015 0%, #D2042D ${val}%, #fff ${val}%, #fff 100%)}`;
+    style += `.range input::-` + prefs[i] + `{background: linear-gradient(to right, #670015 0%, #D2042D ${val}%, #DEDEDE ${val}%, #b2b2b2 100%)}`;
   }
 
   return style;
@@ -37,13 +37,15 @@ rangeInput.forEach(function (input) {
   input.addEventListener('input', function () {
     sheet.textContent = getTrackStyle(this);
   });
+
+  // Initialize style on page load
+  sheet.textContent = getTrackStyle(input);
 });
 
 // Change input value on label click
 var rangeLabels = document.querySelectorAll('.range-labels li');
-rangeLabels.forEach(function (label) {
+rangeLabels.forEach(function (label, index) {
   label.addEventListener('click', function () {
-    var index = Array.from(rangeLabels).indexOf(label);
     rangeInput.forEach(function (input) {
       input.value = index + 1;
       input.dispatchEvent(new Event('input'));
