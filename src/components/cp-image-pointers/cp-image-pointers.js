b91a23a27e6729f716d79830cdb1cae2aaa5be32
Samrudhi-merkle <samrudhi.patil@merkle.com>
2025-01-30 00:39:06 +0530

    cart order confirm pages

--- src/components/cp-image-pointers/cp-image-pointers.js
+++ src/components/cp-image-pointers/cp-image-pointers.js
@@ -1,11 +1,17 @@
 const dots = document.querySelectorAll('.dot');  // Select all dots
 
 dots.forEach(dot => {
+    // For hover (desktop devices)
     dot.addEventListener('mouseover', () => {
         dot.classList.add('open');  // Add 'open' class when mouse is over
     });
 
     dot.addEventListener('mouseleave', () => {
         dot.classList.remove('open');  // Remove 'open' class when mouse leaves
+    });
+
+    // For click (mobile devices)
+    dot.addEventListener('click', () => {
+        dot.classList.toggle('open');  // Toggle 'open' class on click for mobile
     });
 });
