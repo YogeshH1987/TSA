f413a5338bbabdc67aacee2a5dd0dac082546f18
Samrudhi-merkle <samrudhi.patil@merkle.com>
2025-01-31 23:52:54 +0530

    service provider login

---
+++ src/components/cp-service-provider/cp-service-provider.js
@@ -0,0 +1,162 @@
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+    // This function will display the specified tab of the form...
+    var x = document.getElementsByClassName("tab");
+    x[n].style.display = "block";
+
+    // Dynamically update step count
+    var stepCountElement = document.querySelector(".steps-num");
+    stepCountElement.textContent = "Step - " + (n + 1) + "/" + x.length;
+
+    // Fix Previous/Next buttons
+    if (n == 0) {
+        document.getElementById("prevBtn").style.display = "inline";  // Hide prev on the first tab
+    } else {
+        document.getElementById("prevBtn").style.display = "inline";
+    }
+
+    if (n == (x.length - 1)) {
+        // document.getElementById("nextBtn").innerHTML = "Set up my dashboard";
+        document.getElementById("nextBtn").setAttribute("onclick", "submitForm()");
+    } else {
+        document.getElementById("nextBtn").innerHTML = "Next";
+        document.getElementById("nextBtn").setAttribute("onclick", "nextPrev(1)");
+    }
+
+    // Update step indicators
+    fixStepIndicator(n);
+}
+
+function nextPrev(n) {
+    // This function will figure out which tab to display
+    var x = document.getElementsByClassName("tab");
+    var steps = document.getElementsByClassName("step");
+
+    // When moving back (n == -1), remove finish class from the current step and make it active
+    if (n == -1) {
+        // Remove "finish" class from the current step
+        steps[currentTab].classList.remove("finish");
+
+        // Add "active" class to the previous step
+        steps[currentTab].classList.add("active");
+    }
+
+    // Exit the function if any field in the current tab is invalid (only for moving forward)
+    if (n == 1 && !validateForm()) return false;
+
+    // Hide the current tab:
+    x[currentTab].style.display = "none";
+
+    // Increase or decrease the current tab by 1:
+    currentTab = currentTab + n;
+
+    // If you have reached the end of the form...
+    if (currentTab >= x.length) {
+        // ... the form gets submitted:
+        document.getElementById("service-provider-login").submit();
+        return false;
+    }
+    // Otherwise, display the correct tab:
+    showTab(currentTab);
+}
+
+function validateForm() {
+    // This function deals with validation of the form fields
+    var x, y, i, valid = true;
+    x = document.getElementsByClassName("tab");
+    // y = x[currentTab].getElementsByTagName("input");
+
+    // A loop that checks every input field in the current tab:
+    // for (i = 0; i < y.length; i++) {
+    //     // If a field is empty...
+    //     if (y[i].value == "") {
+    //         // Add an "invalid" class to the field:
+    //         y[i].className += " invalid";
+    //         // Set the current valid status to false
+    //         valid = false;
+    //     }
+    // }
+
+    // If the valid status is true, mark the step as finished and valid:
+    if (valid) {
+        document.getElementsByClassName("step")[currentTab].className += " finish";
+    }
+    return valid; // Return the valid status
+}
+
+function fixStepIndicator(n) {
+    // This function removes the "active" class from all steps...
+    var i, x = document.getElementsByClassName("step");
+    for (i = 0; i < x.length; i++) {
+        x[i].className = x[i].className.replace(" active", "");
+        // Remove the "finish" class from the current step if we are going back
+        // x[i].className = x[i].className.replace(" finish", "");
+    }
+
+    // ... and removes the "finish" class from the current step when going back:
+    var currentStep = x[n];
+    currentStep.className = currentStep.className.replace(" finish", "");
+
+    // Add the "active" class to the current step:
+    currentStep.className += " active";
+}
+
+
+function submitForm() {
+    // Perform final validations or data processing here
+    // alert("Form submitted successfully!");
+
+    // Optionally submit the form
+    window.location.href = "#";
+}
+
+
+$(document).ready(function () {
+    // When the 'Organisation' radio button is selected
+    $('#bTyp2').change(function () {
+        if ($(this).prop('checked')) {
+            // Show the organisation section and hide the "Individual" section
+            $('.typ-organisation').removeClass('hide').addClass('show');
+        }
+    });
+
+    // When the 'Individual' radio button is selected
+    $('#bTyp1').change(function () {
+        if ($(this).prop('checked')) {
+            // Hide the organisation section
+            $('.typ-organisation').removeClass('show').addClass('hide');
+        }
+    });
+
+    // When the "Others" radio button is clicked
+    $('#services7').change(function () {
+        if ($(this).prop('checked')) {
+            $('.other-services').removeClass('hide'); // Show the div
+        }
+    });
+
+    // When any other radio button is clicked
+    $('input[name="services"]:not(#services7)').change(function () {
+        $('.other-services').addClass('hide'); // Hide the div
+    });
+});
+
+
+document.getElementById("add-service").addEventListener("click", function() {
+    const servicesContainer = document.getElementById("services-container");
+    const currentInputs = servicesContainer.querySelectorAll("input");
+    
+    // Only allow adding up to 3 inputs
+    if (currentInputs.length < 3) {
+        const newInput = document.createElement("input");
+        newInput.type = "text";
+        newInput.classList.add("form-input", "validate-mobile");
+        newInput.placeholder = "Enter text";
+        newInput.required = true;
+        servicesContainer.appendChild(newInput);
+    } else {
+        alert("You can only add up to 3 services.");
+    }
+});
