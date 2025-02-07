63560890c53c2cd26b13a45a9e313ef37f969305
Samrudhi-merkle <samrudhi.patil@merkle.com>
2025-02-07 19:29:10 +0530

    calculator result page changes

--- src/components/cp-rebar-result/cp-rebar-result.js
+++ src/components/cp-rebar-result/cp-rebar-result.js
@@ -8,3 +8,34 @@ $(function(){
       }
     });
   });
+
+    // Get all the dropdowns
+    const dropdowns = document.querySelectorAll('.select-dropdown');
+
+    dropdowns.forEach(dropdown => {
+        const dropdownButton = dropdown.querySelector('.select-dropdown-button');
+        const dropdownItems = dropdown.querySelectorAll('.select-dropdown-list-item');
+
+        // Event listener for dropdown items
+        dropdownItems.forEach(item => {
+            item.addEventListener('click', () => {
+                const selectedValue = item.getAttribute('data-value');
+                const selectedText = item.innerText;
+
+                // Update the button text
+                dropdownButton.querySelector('.select-text').innerText = selectedText;
+
+                // Remove existing button class
+                dropdownButton.classList.remove('btn-orange', 'btn-pink', 'btn-yellow');
+
+                // Add new button class based on the selection
+                if (selectedValue == 'economy') {
+                    dropdownButton.classList.add('btn-pink');  // Economy price
+                } else if (selectedValue == 'standard') {
+                    dropdownButton.classList.add('btn-orange');  // Standard price
+                } else if (selectedValue == 'premium') {
+                    dropdownButton.classList.add('btn-yellow');  // Premium price
+                }
+            });
+        });
+    });
