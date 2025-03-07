bad9c6cb7ec98e17ec7beb1012719f5b3497dcdb
Samrudhi-merkle <samrudhi.patil@merkle.com>
2025-03-07 19:25:25 +0530

    calculator result image pointer

--- src/components/cp-shed-estimator/cp-shed-estimator.js
+++ src/components/cp-shed-estimator/cp-shed-estimator.js
@@ -1,18 +1,34 @@
 $(document).ready(function () {
     // addedFavorite();
     shedTypeSlider();
+    shedProductSlider()
 });
 
 $(window).resize(function () {
     shedTypeSlider();
+    shedProductSlider()
 });
 
 
 
 function shedTypeSlider() {
-    var swiper = new Swiper(".js-roof-img", {
+    var swiper = new Swiper(".js-shed-type", {
         slidesPerView:1,
         spaceBetween: 20,
+        loop: false,
+        navigation:false,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+
+    });
+}
+
+function shedProductSlider() {
+    var swiper = new Swiper(".js-shed-product", {
+        slidesPerView:2.5,
+        spaceBetween: 12,
         loop: false,
         navigation:false,
         pagination: {
