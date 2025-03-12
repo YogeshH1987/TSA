62a0dc50dec5aa60ecc7e2d48b54a0d35161118d
Samrudhi-merkle <samrudhi.patil@merkle.com>
2025-03-12 19:44:30 +0530

    budget tracker

--- src/components/cp-product-type/cp-product-type.js
+++ src/components/cp-product-type/cp-product-type.js
@@ -1,5 +1,6 @@
 $(document).ready(function () { 
   productTypeSlider();  
+  productMaterialSlider()
 });
 
 
@@ -9,4 +10,28 @@ function productTypeSlider() {
         loop: false,
         navigator: false
     });
+}
+
+
+function productMaterialSlider() {
+  var swiper = new Swiper(".js-product-material", {
+      slidesPerView: 3.2,
+      spaceBetween: 12,
+      loop: false,
+      navigation:false,
+      pagination:false,
+
+      breakpoints: {
+          // when window width is <= 499px
+          768: {
+              slidesPerView: 3.2,
+              spaceBetweenSlides: 12,
+          },
+          // when window width is <= 999px
+          999: {
+              slidesPerView: 8,
+              spaceBetweenSlides: 20
+          }
+      }
+  });
 }
